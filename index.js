d58a3ebed25f28f47124c654accea0420a8de25a
ubuntu <djdidoussama@gmail.com>
2024-10-25 09:19:09 +0100

    fix error cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors"); // Import cors
+const cors = require("cors");
 const connectDB = require("./config/db");
 const projectRoutes = require("./routes/projectRoutes");
 const blogPostsRoutes = require("./routes/blogPosts");
@@ -8,7 +8,6 @@ const servicesRoutes = require("./routes/services");
 const userRoutes = require("./routes/user");
 const ErrorHandler = require("./middlewares/error");
 const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 const cloudinary = require("cloudinary");
 
 dotenv.config();
@@ -17,17 +16,16 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(bodyParser.json({ limit: "50mb" }));
-// CORS Middleware
+
+// Middleware
 app.use(
   cors({
-    origin: ["https://it-company-front.vercel.app/"],
+    origin: "https://it-company-front.vercel.app", // Make sure there are no trailing slashes
     credentials: true,
   })
 );
-// Middleware
-app.use(cookieParser()); // for parsing cookies
-app.use(express.json({ limit: "50mb" })); // Increase the limit for JSON payload
+app.use(cookieParser());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 cloudinary.config({
@@ -46,10 +44,11 @@ app.use("/api/blogs", blogPostsRoutes);
 app.use("/api/services", servicesRoutes);
 app.use("/api/users", userRoutes);
 
+// Error Handling
+app.use(ErrorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-app.use(ErrorHandler);
